9996ccd61e74651dd80a8399535271d47deb7d0e
Eric Klawitter <esklawitter@gmail.com>
2015-09-26 03:34:45 -0400

    Created dummy data for tasks / task details

---
+++ api/dummydata.js
@@ -0,0 +1,38 @@
+task= {
+	details:{
+		title: "Spread 12 bags of mulch",
+		category: "landscaping",
+		time: "2.5",
+		pay: "20.25",
+		taskID: 223156
+	},
+	body: {
+		pics: ["pretend there is data here", "pretend there is data here"],
+		userID: 123343,
+		applied: [11234, 345433, 345121]
+	}
+};
+
+taskDetails= [{
+	details:{
+		title: "Spread 12 bags of mulch",
+		category: "landscaping",
+		time: "2.5",
+		pay: "20.25",
+		taskID: 223156
+	},{
+	details:{
+		title: "Spread 12 bags of mulch",
+		category: "landscaping",
+		time: "2.5",
+		pay: "20.25",
+		taskID: 223156
+	},{
+	details:{
+		title: "Spread 12 bags of mulch",
+		category: "landscaping",
+		time: "2.5",
+		pay: "20.25",
+		taskID: 223156
+	}];
+	
