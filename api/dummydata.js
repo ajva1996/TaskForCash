92ae8e321502ab68cf37dc0c1193069288b400f2
Eric Klawitter <esklawitter@gmail.com>
2015-09-26 03:56:50 -0400

    dummy data for users

--- api/dummydata.js
+++ api/dummydata.js
@@ -7,7 +7,7 @@ task= {
 		taskID: 223156
 	},
 	body: {
-		pics: ["pretend there is data here", "pretend there is data here"],
+		pics: ["http://i.imgur.com/7NAzFyg.png", "http://i.imgur.com/7NAzFyg.png"],
 		userID: 123343,
 		applied: [11234, 345433, 345121]
 	}
@@ -35,4 +35,61 @@ taskDetails= [{
 		pay: "20.25",
 		taskID: 223156
 	}];
-	
+
+adult={
+	personalInfo : {
+		name: "Jon Doe",
+		age: 53,
+		gender: "Male",
+		bio: "lorem ipsum dolor asdf asdf asdf asdf asdf asdf asdf asdfasd fasdf",
+		userID: "123443"
+	}
+	location : {
+		geocode: "1234123019237af;asldkf",
+		address: "1600 Pennsylvania Ave",
+		city: "Atlanta",
+		zip: "30313"
+	}
+	jobListings: [12341234, 23412123, 1231238],
+	feedback: [{
+		clarity: 3,
+		respect: 5,
+		followthrough: 2
+	},
+	{
+		clarity: 3,
+		respect: 5,
+		followthrough: 2
+	}];
+	picture: "http://i.imgur.com/7NAzFyg.png"
+	isKid: false,
+};
+
+kid={
+	personalInfo : {
+		name: "Joey Doe",
+		age: 53,
+		gender: "Male",
+		bio: "lorem ipsum dolor asdf asdf asdf asdf asdf asdf asdf asdfasd fasdf",
+		userID: "123443"
+	}
+	location : {
+		geocode: "1234123019237af;asldkf",
+		address: "1600 Pennsylvania Ave",
+		city: "Atlanta",
+		zip: "30313"
+	}
+	jobListings: [12341234, 23412123, 1231238],
+	feedback: [{
+		attitude: 3,
+		punctual: 5,
+		quality: 2
+	},
+	{
+		attitude: 3,
+		punctual: 5,
+		quality: 2
+	}];
+	picture: "http://i.imgur.com/7NAzFyg.png"
+	isKid: true,
+};
