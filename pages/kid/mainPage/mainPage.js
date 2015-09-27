28cd5356f7988ad0bb6308ead90ceb2d617de506
Eric Klawitter <esklawitter@gmail.com>
2015-09-27 10:03:00 -0400

    final js changes, filtering by distance

--- pages/kid/mainPage/mainPage.js
+++ pages/kid/mainPage/mainPage.js
@@ -1,3 +1,5 @@
+var taskData;
+var curDataSet;
 $(document).ready( function(){
     init();
 
@@ -12,15 +14,16 @@ $(document).ready( function(){
             $(".headerText.red")[0].style.display="none";
             $(".headerTree.red")[0].style.display="none";
             $(".headerText.light")[0].style.display="block";
+
         });
         $.ajax({
             type: "GET",
             url: "http://52.11.205.176:8080/api/task",
             data: {hi:"hello from client"},
             success: function(response) {
-                console.log(response)
-                //console.log(response);
                 populatePanels(response);
+                taskData=response;
+                curDataSet=response;
                 populateCheckboxes(response);
 
             },
@@ -31,43 +34,6 @@ $(document).ready( function(){
         });
 
     }
-    function populatePanels(data){
-        var parent= document.getElementById("tasksContainer");
-        for (var i = 0; i < data.length; i++){
-            //var temp= document.getElementById("panelTemplate").cloneNode(true);
-            var temp= $("#panelTemplate").clone(true);
-            populateClone(temp, data[i]);
-            parent.appendChild(temp[0]);
-
-        }
-    }
-    function populateClone(node, data){
-        node.find('#paySpan')[0].innerText=data.pay;
-        node.find('#timeSpan')[0].innerText=data.time;
-        node.find('#distSpan')[0].innerText=data.distance;
-        node.find("#titleContainer")[0].innerText=data.title;
-        node.find("#categoryContainer")[0].innerText=data.category;
-        node[0].details=data;
-        node[0].style.zIndex=4;
-    }
-    function populateCheckboxes(data){
-        categories={};
-        for (var i = 0; i < data.length; i++){
-            categories[data[i].category]= data[i].category;
-        }
-        var parent= $("#checkboxContainer");
-        var node= $("#checkboxTemplate");
-        for (var property in categories) {
-            if (categories.hasOwnProperty(property)) {
-                var temp= node.clone(true);
-                var a= document.createElement("SPAN");
-                a.innerText= categories[property];
-                temp.find("#checkboxLabel")[0].appendChild(a);
-                parent[0].appendChild(temp[0]);
-            }
-        }
-    }
-
 
 
 });
@@ -75,3 +41,55 @@ function navigateToDetails(item){
     //console.log(item.details);
     location.href= "../../kid/taskPage/taskPage.html" + "?"+ item.details.pay+item.details.time;
 }
+function populatePanels(data){
+    $("#tasksContainer").empty();
+    var parent= document.getElementById("tasksContainer");
+    for (var i = 0; i < data.length; i++){
+        //var temp= document.getElementById("panelTemplate").cloneNode(true);
+        var temp= $("#panelTemplate").clone(true);
+        populateClone(temp, data[i]);
+        parent.appendChild(temp[0]);
+
+    }
+}
+function populateClone(node, data){
+    node.find('#paySpan')[0].innerText=data.pay;
+    node.find('#timeSpan')[0].innerText=data.time;
+    node.find('#distSpan')[0].innerText=data.distance;
+    node.find("#titleContainer")[0].innerText=data.title;
+    node.find("#categoryContainer")[0].innerText=data.category;
+    node[0].details=data;
+    node[0].style.zIndex=4;
+}
+function populateCheckboxes(data){
+    categories={};
+    for (var i = 0; i < data.length; i++){
+        categories[data[i].category]= data[i].category;
+    }
+    var parent= $("#checkboxContainer");
+    var node= $("#checkboxTemplate");
+    for (var property in categories) {
+        if (categories.hasOwnProperty(property)) {
+            var temp= node.clone(true);
+            var a= document.createElement("SPAN");
+            a.innerText= categories[property];
+            temp.find("#checkboxLabel")[0].appendChild(a);
+            parent[0].appendChild(temp[0]);
+        }
+    }
+}
+
+function updateFilters(){
+    curDataSet=JSON.parse(JSON.stringify(taskData));
+    var filter=$("#sel1 option:selected" ).text();
+    filter= parseInt(filter);
+    for (var i = 0; i < curDataSet.length; i++){
+        if (parseFloat(curDataSet[i].distance) > filter){
+            curDataSet.splice(i, 1);
+            i--;
+        }
+    }
+    var boxes= $("#categoryCheckbox");
+    populatePanels(curDataSet);
+}
+
